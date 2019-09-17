7358e8d6c98dbbe0dc0233da04d7acce02d51815
Vijay P V <vijayraj1998@yahoo.com>
2019-09-17 18:02:15 +0530

    Add files via upload

---
+++ Data Science/SVM/svm.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn import svm 
+import numpy as np 
+from sklearn.metrics import confusion_matrix 
+from sklearn.model_selection import train_test_split  
+from sklearn.metrics import accuracy_score 
+from sklearn.metrics import classification_report 
+
+balance_data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-'+'databases/balance-scale/balance-scale.data',	sep= ',', header = None) 
+# Printing the dataswet shape 
+print ("Dataset Length: ", len(balance_data)) 
+print ("Dataset Shape: ", balance_data.shape) 
+	
+# Printing the dataset obseravtions 
+print ("Dataset: ",balance_data.head()) 
+X = balance_data.values[:, 1:5] 
+Y = balance_data.values[:, 0] 
+
+# Spliting the dataset into train and test 
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 100) 
+
+#Creating a gini based model
+model = svm.SVC() 
+
+# Actual training process 
+model.fit(X_train, y_train) 
+
+# Predicton on testdata 
+y_pred = model.predict(X_test) 
+print("Predicted values:") 
+print(y_pred)
+
+print("Confusion Matrix:\n ", confusion_matrix(y_test, y_pred)) 
+	
+print ("Accuracy : ", accuracy_score(y_test,y_pred)*100) 
+	
+print("Report : ", classification_report(y_test, y_pred))
+	
