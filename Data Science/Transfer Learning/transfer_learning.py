e45755fd459824058f1d8fba5211c4fcb22198c3
Vijay P V <vijayraj1998@yahoo.com>
2019-09-19 15:10:54 +0530

    Add files via upload

---
+++ Data Science/Transfer Learning/transfer_learning.py
@@ -0,0 +1,52 @@
+# Save Model Using joblib
+import pandas
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
+import pandas as pd
+from sklearn import svm 
+import numpy as np 
+from sklearn.metrics import confusion_matrix 
+from sklearn.model_selection import train_test_split  
+from sklearn.metrics import accuracy_score 
+from sklearn.metrics import classification_report 
+
+balance_data = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-'+'databases/balance-scale/balance-scale.data',	sep= ',', header = None) 
+# Printing the dataswet shape 
+print ("Dataset Length: ", len(balance_data)) 
+print ("Dataset Shape: ", balance_data.shape) 
+	
+# Printing the dataset obseravtions 
+print ("Dataset: ",balance_data.head()) 
+X = balance_data.values[:, 1:5] 
+Y = balance_data.values[:, 0] 
+
+# Spliting the dataset into train and test 
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 100) 
+
+#Creating a gini based model
+model = svm.SVC() 
+
+# Actual training process 
+model.fit(X_train, y_train) 
+
+# Predicton on testdata 
+y_pred = model.predict(X_test) 
+print("Predicted values:") 
+print(y_pred)
+
+print("Confusion Matrix:\n ", confusion_matrix(y_test, y_pred)) 
+	
+print ("Accuracy : ", accuracy_score(y_test,y_pred)*100) 
+	
+print("Report : ", classification_report(y_test, y_pred))
+# save the model to disk
+filename = 'finalized_model.sav'
+joblib.dump(model, filename)
+
+# some time later...
+
+# load the model from disk
+loaded_model = joblib.load(filename)
+result = loaded_model.score(X_test, y_test)
+print(result)
